4c65327a75e84f3de71b7ee4100091a3f18a76e8
James Huston <jamzhuston@gmail.com>
2025-07-25 10:20:53 -0600

    created all files that I will use for this project

---
+++ src/paddle.rs
@@ -0,0 +1,52 @@
+use crate::pong_types::*;
+use macroquad::prelude::*;
+
+pub struct Paddle {
+    width: f32,
+    height: f32,
+    x: f32,
+    y: f32,
+    speed: f32,
+    ctrl_up: KeyCode,
+    ctrl_down: KeyCode,
+    color: Color,
+}
+
+pub fn init_paddle(
+    dimensions: (f32, f32),
+    coords: (f32, f32),
+    speed: f32,
+    controls: (KeyCode, KeyCode),
+    color: Color,
+) -> Paddle {
+    Paddle {
+        width: dimensions.0,
+        height: dimensions.1,
+        x: coords.0,
+        y: coords.1,
+        speed,
+        ctrl_up: controls.0,
+        ctrl_down: controls.1,
+        color,
+    }
+}
+
+impl Paddle {}
+
+impl Update for Paddle {
+    fn update(self: &mut Paddle, dt: f32) {
+        let distance = self.speed * dt;
+        if is_key_down(self.ctrl_up) {
+            self.y += distance;
+        }
+        if is_key_down(self.ctrl_down) {
+            self.y -= distance
+        }
+    }
+}
+
+impl Draw for Paddle {
+    fn draw(self: &Paddle) {
+        draw_rectangle(self.x, self.y, self.width, self.height, self.color);
+    }
+}
