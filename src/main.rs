690983c41cbca9b55cd985bcac944e147ad09c96
James Huston <jamzhuston@gmail.com>
2025-07-27 16:56:38 -0600

    fixed paddle initialization

--- src/main.rs
+++ src/main.rs
@@ -3,40 +3,35 @@ mod gamestate;
 mod paddle;
 mod pong_types;
 mod score;
-use macroquad::prelude::*;
+mod settings;
 
-use crate::{paddle::init_paddle, pong_types::Draw, pong_types::Update};
+use crate::paddle::*;
+use crate::pong_types::*;
+use macroquad::prelude::*;
 
 #[macroquad::main("Pong")]
 async fn main() {
-    let player_width: f32 = 32.;
-    let player_height: f32 = 128.;
-    let player_dimensions: (f32, f32) = (player_width, player_height);
     let player_speed: f32 = 250.;
     let player_x_offset: f32 = 16.;
     let player_y_coord: f32 = screen_height() / 2.0;
     let play_game: bool = true;
-    // will be left player
+    // LEFT PLAYER
     let mut p1 = init_paddle(
-        player_dimensions,
-        (
-            player_x_offset,
-            (screen_height() / 2.0) - (player_height / 2.0),
-        ),
+        Vec2 {
+            x: player_x_offset,
+            y: player_y_coord - (PADDLE_HEIGHT / 2.0),
+        },
         player_speed,
         (KeyCode::S, KeyCode::W),
-        BLUE,
     );
-    // will be right player
+    // RIGHT PLAYER
     let mut p2 = init_paddle(
-        player_dimensions,
-        (
-            screen_width() - player_width - player_x_offset,
-            (screen_height() / 2.0) - (player_height / 2.0),
-        ),
+        Vec2 {
+            x: screen_width() - PADDLE_WIDTH - player_x_offset,
+            y: player_y_coord - (PADDLE_HEIGHT / 2.0),
+        },
         player_speed,
         (KeyCode::Down, KeyCode::Up),
-        BLUE,
     );
 
     // main game loop
