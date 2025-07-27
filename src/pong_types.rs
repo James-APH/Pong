8cedae2901cd64f7467617f87742e41e77bf1de5
James Huston <jamzhuston@gmail.com>
2025-07-27 16:57:39 -0600

    added pong color

--- src/pong_types.rs
+++ src/pong_types.rs
@@ -1,5 +1,8 @@
+use macroquad::prelude::*;
+
 pub const PADDLE_HEIGHT: f32 = 128.;
 pub const PADDLE_WIDTH: f32 = 32.;
+pub const PADDLE_COLOR: Color = RED;
 
 pub trait Update {
     fn update(&mut self, dt: f32);
