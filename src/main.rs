e3a028493381119f205f77d34138a970d8b4e745
James Huston <jamzhuston@gmail.com>
2025-08-13 09:05:34 -0700

    renamed variables, removed magic numbers

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod pong_types;
 mod score;
 mod settings;
 
-use crate::ball::init_ball;
+use crate::ball::*;
 use crate::collisions::*;
 use crate::paddle::*;
 use crate::pong_types::*;
@@ -15,37 +15,42 @@ use macroquad::prelude::*;
 
 #[macroquad::main("Pong")]
 async fn main() {
-    let player_speed: f32 = 250.;
+    let player_vel: f32 = 250.;
+    let initial_ball_vel: f32 = 75.;
     let player_x_offset: f32 = 16.;
     let mid_screen_y: f32 = screen_height() / 2.0;
     let mid_screen_x: f32 = screen_width() / 2.0;
+    let game_start_player_y_coord = mid_screen_y - (PADDLE_HEIGHT / 2.0);
     let play_game: bool = true;
-    // LEFT PLAYER
-    let mut p1 = init_paddle(
+
+    let mut left_player = init_paddle(
         Vec2 {
             x: player_x_offset,
-            y: mid_screen_y - (PADDLE_HEIGHT / 2.0),
+            y: game_start_player_y_coord,
         },
-        player_speed,
+        player_vel,
         (KeyCode::S, KeyCode::W),
     );
-    // RIGHT PLAYER
-    let mut p2 = init_paddle(
+
+    let mut right_player = init_paddle(
         Vec2 {
             x: screen_width() - PADDLE_WIDTH - player_x_offset,
-            y: mid_screen_y - (PADDLE_HEIGHT / 2.0),
+            y: game_start_player_y_coord,
         },
-        player_speed,
+        player_vel,
         (KeyCode::Down, KeyCode::Up),
     );
-    // BALL
+
     let mut ball = init_ball(
         Vec2 {
             x: mid_screen_x,
             y: mid_screen_y,
         },
-        Vec2 { x: 20.0, y: 20.0 },
-        Vec2 { x: 1.0, y: -1.0 },
+        Vec2 {
+            x: initial_ball_vel,
+            y: initial_ball_vel,
+        },
+        Vec2 { x: 1.0, y: 1.0 },
     );
 
     // main game loop
@@ -55,18 +60,18 @@ async fn main() {
         clear_background(GRAY);
         if play_game {
             ball.update(delta_time);
-            p1.update(delta_time);
-            p2.update(delta_time);
+            left_player.update(delta_time);
+            right_player.update(delta_time);
 
             bounce_ball_at_wall(&mut ball, screen_height());
-            is_ball_at_paddle(&ball, &p1);
-            bounce_ball(&mut ball, &p1);
-            is_ball_at_paddle(&ball, &p2);
-            bounce_ball(&mut ball, &p2);
+            is_ball_at_paddle(&ball, &left_player);
+            bounce_ball(&mut ball, &left_player);
+            is_ball_at_paddle(&ball, &right_player);
+            bounce_ball(&mut ball, &right_player);
         }
 
-        p1.draw();
-        p2.draw();
+        left_player.draw();
+        right_player.draw();
         ball.draw();
         next_frame().await
     }
